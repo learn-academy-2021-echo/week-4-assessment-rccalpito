527901903d535b9fccfa7a877b567af441f70ab3
rccalpito <rccalpito@gmail.com>
2022-01-09 22:12:13 -0800

    completed most of the assignment. going to finish tomorrow

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,39 +12,90 @@
 
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
+// use a function similar to .shift() or .pop() to remove an element within the array
+// .shift removes the first element of an array and returns that removed element
+// randomizer is Math.floor(Math.random() * array.length)
+// 
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// describe("remRandArr", () => {
 
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//     it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+  
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//       expect(remRandArr(colors1)).toEqual(["yellow", "blue", "pink", "green"])
+//       expect(remRandArr(colors2)).toEqual(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"])
+//     })
+//   })
 var colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// console.log(colors1.pop(colors1[0]))
 
 
 // b) Create the function that makes the test pass.
+// this would work in a perfect world 
 
+    // remRandArr = (arr) => {
+    //     arr.reverse().pop()
+    //     const newArr = []
+    //     while (newArr.length < arr.length){
+    //         if (newArr.includes(arr[Math.floor(Math.random() * arr.length)])){
+    //             newArr.push(arr[Math.floor(Math.random() * arr.length)])
+    //         }
+    //     }
+    //     return console.log(newArr)
+    // }
+    
 
-
+    // remRandArr(colors1)
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("minMax", () => {
 
-var nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-var nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(minMax(nums1)).toEqual([-8, 90])
+      expect(minMax(nums2)).toEqual([5, 109])
+    })
+  })
+// var nums1 = [3, 56, 90, -8, 0, 23, 6]
+// // Expected output: [-8, 90]
+// var nums2 = [109, 5, 9, 67, 8, 24]
+// // Expected output: [5, 109]
 
 
 
 // b) Create the function that makes the test pass.
+// using min and max method push into a new array.. i think thats it..
+
+minMax = (numArr) => {
+    newArr = []
+    newArr.push((Math.min(...numArr)))
+    newArr.push((Math.max(...numArr)))
+    return newArr
+}
 
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+describe("repeatNum", () => {
 
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("function that takes in two arrays as arguments and returns one array with no duplicate values", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(repeatNum(testArray1, testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
+  })
 var testArray1 = [3, 7, 10, 5, 4, 3, 3]
 var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
@@ -52,3 +103,14 @@ var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+//[...new Set(array)] creates a unique "set" or array
+// concat both arrays into one big papa array, and use the set function to create a unique array (with no repeating values)
+
+repeatNum = (arr1,arr2) => {
+
+    let newArr = [...new Set(arr1.concat(arr2))]
+    return newArr
+
+}
+
--- interview-questions.md
+++ interview-questions.md
@@ -7,7 +7,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: Props in react are "properties" that are passed from a parent component to a child component. These props can pass both data and behavior to a child component. These passed properties are only one directional, and cannot be passed from child to parent. In order to pass said behavior or data to a component, the function/state variable must be passed using this.props.
 
   Researched answer:
 
@@ -15,7 +15,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: The DOM is a document object manager. This is the display that is refreshed and updated that a user sees. A DOM event is an event that happens on the screen that causes a re-rendering of what is being displayed.
 
   Researched answer:
 
@@ -23,7 +23,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: Object oriented programming is the methodology that the programs be thought of as data driven (objects), instead of behavior driven (functions). 
 
   Researched answer:
 
@@ -31,7 +31,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: An integer is a whole number value. A float is a decimal value that can be represented up to a certain number of bits. 
 
   Researched answer:
 
@@ -39,7 +39,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: An implicit return is such that functions do not to return explicit values. 
 
   Researched answer:
 
